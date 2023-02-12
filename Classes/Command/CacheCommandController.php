5f14c5f5f1fffa7146a2f00c1ae6bb20c88f76ec
Jon Uhlmann <account@uhlmann.pro>
2023-02-12 18:57:28 +0100

    Update CacheCommandController.php

--- Classes/Command/CacheCommandController.php
+++ Classes/Command/CacheCommandController.php
@@ -4,7 +4,9 @@ namespace Carbon\IncludeAssetsCache\Command;
 
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Cli\CommandController;
+use Neos\Flow\Log\Utility\LogEnvironment;
 use Neos\Fusion\Core\Cache\ContentCache;
+use Psr\Log\LoggerInterface;
 
 /**
  *
@@ -19,6 +21,12 @@ class CacheCommandController extends CommandController
     protected $contentCache;
 
     /**
+     * @Flow\Inject
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    /**
      * Clear cache for include assets
      *
      * @return void
@@ -29,9 +37,21 @@ class CacheCommandController extends CommandController
 
         if ($numberOfEntries === 0) {
             $this->outputFormatted('No entries found in the include assets cache.');
+            $this->logger->debug(
+                'No entries found in the include assets cache.',
+                LogEnvironment::fromMethodName(__METHOD__)
+            );
             return;
         }
         $entriesPlural = $numberOfEntries === 1 ? 'entry' : 'entries';
         $this->outputFormatted('<success><b>%s %s</b> removed from the include assets cache.</success>', [$numberOfEntries, $entriesPlural]);
+        $this->logger->debug(
+            sprintf(
+                '%s %s removed from the include assets cache.',
+                $numberOfEntries,
+                $entriesPlural
+            ),
+            LogEnvironment::fromMethodName(__METHOD__)
+        );
     }
 }
